a499a6692ab353d84c59b77fab62f569a967cc07
Adam Demuri <adam.demuri@gmail.com>
2021-07-01 20:25:33 -0600

    Make speed test more space efficient

--- speed-test/speed-test.py
+++ speed-test/speed-test.py
@@ -5,34 +5,36 @@ spacing = 10
 safe_height = 5
 plunge_speed = 100
 
-speeds = [1600, 2000, 2400, 2800, 3200]
+speeds = [1600, 2000, 2400, 2800, 3200, 3600, 4000]
 
 f.write("; Speed test\n\n")
 f.write("G21\n")
 forward = True
 
-x = 0
-y = distance
+x = spacing
+y = 0
 f.write(f"G00 Z{safe_height}\n")
 f.write(f"G00 X{x} Y{y}\n\n")
 
 for speed in speeds:
     f.write(f"G01 Z0 F{plunge_speed}\n")
     if forward:
-        y -= distance
+        x -= spacing
     else:
-        x -= distance
+        x += spacing
     f.write(f"G01 X{x} Y{y} F{speed}\n")
 
     if forward:
-        x += distance
-    else:
         y += distance
+    else:
+        y -= distance
     f.write(f"G01 X{x} Y{y} F{speed}\n")
     f.write(f"G00 Z{safe_height}\n")
 
-    x += spacing
-    y += spacing
+    if forward:
+        x += spacing / 2
+    else:
+        x += spacing * 1.5
 
     f.write(f"G00 X{x} Y{y}\n\n")
     forward = not forward
